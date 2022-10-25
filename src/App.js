08473f702ccebe7b99db3c9e79e490dad0491714
YasanLakmalH <yasanhemachandra@gmail.com>
2022-10-25 11:26:12 +0530

    local storage feature added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState,useEffect } from "react";
 
 function App() {
   const [selectedTeam, setTeam] = useState(JSON.parse(localStorage.getItem('selectedTeam')) || "TeamA");
-  const [employees, setEmployees] = useState([{
+  const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) ||[{
       id: 1,
       imgUrl: "img/male.png",
       name: "Member-1",
@@ -51,15 +51,13 @@ function App() {
     },
   ]);
 
-
-
-
-  
   useEffect(()=>{
-
     localStorage.setItem('selectedTeam',JSON.stringify(selectedTeam));
-
   },[selectedTeam]);
+
+  useEffect(()=>{
+    localStorage.setItem('employeeList',JSON.stringify(employees));
+  },[employees]);
 
 
 
@@ -68,7 +66,7 @@ function App() {
   };
 
   const handleEmployeeCardClick = (event) => {
-   
+    
     const transformedEmployees = employees.map((employee) =>(
       employee.id === parseInt(event.currentTarget.id)
         ? 
@@ -80,11 +78,13 @@ function App() {
     );
     setEmployees(transformedEmployees);
   };
+
   return (
     <div className="App">
       <Header
       selectedTeam={selectedTeam}
-      teamMemberCount={employees.filter((employee)=>employee.teamName === selectedTeam).length}
+      teamMemberCount={
+        employees.filter((employee)=>employee.teamName === selectedTeam).length}
       />
       <main>
         <div className='container col-8'>
